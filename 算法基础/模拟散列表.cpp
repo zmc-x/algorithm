82b24d25e2f4ffecfa387030d26d2c0804893966
hellozmc <3106943528@qq.com>
2022-07-28 12:16:27 +0800

    加入开放寻址法

--- 算法基础/模拟散列表.cpp
+++ 算法基础/模拟散列表.cpp
@@ -1,4 +1,5 @@
 /*
+    拉链法
     首先这一题是Hash表的一个典型的题目，本题采用拉链法来实现对数据的
 存储，首先通过mod来取得再主串中的position，然后通过静态链表的方式来
 进行相关的存储即可
@@ -44,6 +45,46 @@ int main()
         }
         else{
             query(x);
+        }
+    }
+    return 0;
+}
+/*
+    开放寻址法
+    首先这种方法比较简单，只需要开辟一个数组空间就可以完成了。
+这种方法就类似于上厕所，寻找坑位的过程。如果此时位置存在位置，那么就不断向后寻找，
+直到找到空的地方位置，值得注意的是需要找一个不在数据范围的数进行相关的标志操作。
+而核心就是find function，
+*/
+#include<iostream>
+using namespace std;
+extern const int N = 3e5 + 17;
+extern const long long Flag = 1e9 + 1;
+// 初始化
+long long h[N];
+// return the position
+int find(int x)
+{
+    int pos{0};
+    pos = ((x % N) + N) % N;
+    while(h[pos] != Flag && h[pos] != x) pos = (pos + 1) % N;
+    return pos;
+}
+int main()
+{
+    for(int i = 0; i < N; i++) h[i] = Flag;
+    char op;
+    int x{0}, n{0}, k{0};
+    cin >> n;
+    while(n--){
+        cin >> op >> x;
+        k = find(x);
+        if(op == 'I'){
+            h[k] = x;
+        }
+        else{
+            if(h[k] == Flag) cout << "No" << endl;
+            else cout << "Yes" << endl;
         }
     }
     return 0;
